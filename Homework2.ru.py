1f740080d5d5f0e631b29e68f985206a9ed45851
nesterov9 <a.nesterov3@vk.com>
2024-07-24 17:19:24 +0300

    Add files via upload

---
+++ Homework2.ru.py
@@ -0,0 +1,5 @@
+the_number_of_completed_dz =12
+number_of_hours_spent=1.5
+course_name=('курс phython')
+time_for_one_task=(number_of_hours_spent/the_number_of_completed_dz)
+print(course_name,'всего задач',the_number_of_completed_dz,'затрачено часов',number_of_hours_spent,'среднее время выполнения',time_for_one_task)
